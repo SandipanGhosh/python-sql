209937e22614d4c4f843208aa6212e7a965cf4b5
SandipanGhosh <sandipan.ghosh2005@gmail.com>
2018-01-22 15:51:25 -0500

    Small app that performs aggregations on 100 integers based on user input.

---
+++ sql-insert.py
@@ -0,0 +1,11 @@
+import sqlite3
+import random
+
+with sqlite3.connect("newnum.db") as connection:
+	cursor = connection.cursor()
+
+	cursor.execute("DROP TABLE if exists numbers")
+	cursor.execute("CREATE TABLE numbers(num int)")
+
+	for i in range(100):
+		cursor.execute("INSERT INTO numbers VALUES(?)", (random.randint(0,100),))
