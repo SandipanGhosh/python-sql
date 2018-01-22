fc42cbd97e2dc6ded4f99cb2dc93db535d7bfa67
SandipanGhosh <sandipan.ghosh2005@gmail.com>
2018-01-22 13:41:30 -0500

    Multiple tables and JOIN

---
+++ 09_sql.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+	cursor = connection.cursor()
+
+	cursor.execute("""CREATE TABLE regions
+					(city TEXT, region TEXT)
+					""")
+
+	# insert multiple values
+	cities = [
+				('New York City', 'Northeast'),
+				('San Francisco', 'West'),
+				('Chicago', 'Midwest'),
+				('Houston', 'South'),
+				('Phoenix', 'West'),
+				('Boston', 'Northeast'),
+				('Los Angeles', 'West'),
+				('Houston', 'South'),
+				('Philadelphia', 'Northeast'),
+				('San Antonio', 'South'),
+				('San Diego', 'West'),
+				('Dallas', 'South'),
+				('San Jose', 'West'),
+				('Jacksonville', 'South'),
+				('Indianapolis', 'Midwest'),
+				('Austin', 'South'),
+				('Detroit', 'Midwest')
+			]
+
+	cursor.executemany("INSERT INTO regions VALUES(?, ?)", cities)
+
+	cursor.execute("SELECT * FROM regions ORDER BY region ASC")
+	rows = cursor.fetchall()
+	for r in rows:
+		print(r[0], r[1])
