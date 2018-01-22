0403add52c390033c42d81cd82aa327e6c2c782b
SandipanGhosh <sandipan.ghosh2005@gmail.com>
2018-01-22 13:52:12 -0500

    Functions

---
+++ 11_sql.py
@@ -0,0 +1,21 @@
+# SQLite functions
+
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+	cursor = connection.cursor()
+
+	# create a dictionary of sql queries
+	sql = {
+			'average': "SELECT avg(population) FROM population",
+			'maximum': "SELECT max(population) FROM population",
+			'minimum': "SELECT min(population) FROM population",
+			'sum': "SELECT sum(population) FROM population",
+			'count': "SELECT count(city) FROM population"
+		}
+
+	# run each sql query item in the dictionary
+	for keys, values in sql.items():
+		cursor.execute(values)
+		result = cursor.fetchone()
+		print(keys + ":", result[0])
